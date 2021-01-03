245e600bbce1d430bd799d4cd1e003c503b685be
ahnsv <ahnsv@bc.edu>
2021-01-04 00:14:27 +0900

    airflow values와 nginx ingress를 업데이트한다

--- dags/first_dag/dag.py
+++ dags/first_dag/dag.py
@@ -1,6 +1,9 @@
 from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.utils.dates import days_ago
 
 with DAG(dag_id="first_dag", start_date=days_ago(2), schedule_interval="@once") as dag:
-    first_Dag_task = DummyOperator(task_id="first_dag_task")
+    first_dag_task_1 = DummyOperator(task_id="first_dag_task")
+    first_dag_task_2 = BashOperator(task_id="first_dag_task_2", bash_command="sleep 5")
+    first_dag_task_1 >> first_dag_task_2
