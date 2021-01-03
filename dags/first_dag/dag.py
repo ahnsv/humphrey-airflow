5b1bc12c94289fde8cdf05d42766bf8aedbf122d
ahnsv <ahnsv@bc.edu>
2021-01-04 00:41:57 +0900

    persistence를 제거한다

--- dags/first_dag/dag.py
+++ dags/first_dag/dag.py
@@ -5,5 +5,5 @@ from airflow.utils.dates import days_ago
 
 with DAG(dag_id="first_dag", start_date=days_ago(2), schedule_interval="@once") as dag:
     first_dag_task_1 = DummyOperator(task_id="first_dag_task")
-    first_dag_task_2 = BashOperator(task_id="first_dag_task_2", bash_command="sleep 5")
+    first_dag_task_2 = BashOperator(task_id="first_dag_task_2", bash_command="wait 5")
     first_dag_task_1 >> first_dag_task_2
